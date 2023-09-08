745f3e233de1981ee1ee3a84a0cb8d49effbe946
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-08 13:46:50 -0300

    Inserindo usuarios no SCRIPT SQL

--- script.sql
+++ script.sql
@@ -18,3 +18,5 @@ CREATE TABLE `formulario_database_php`.`usuarios` (
 ALTER TABLE `usuarios` CHANGE `sexo` `genero` VARCHAR(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL;
 
 ALTER TABLE `usuarios` ADD `senha` VARCHAR(20) NOT NULL AFTER `nome`;
+
+INSERT INTO `usuarios` (`id`, `nome`, `senha`, `email`, `telefone`, `genero`, `data_nasc`, `cidade`, `estado`, `endereco`) VALUES (NULL, 'Fabiana', '22446688', 'fabiana@gmail.com', '112123255', 'feminino', '2023-09-01', 'SP', 'Sao Paulo', 'Rua da Fabi'), (NULL, 'Leticia Rocha', '123456', 'lerocha@gmail.com', '1123454789', 'feminino', '2023-09-01', 'Ferraz de Vasconcelos', 'SP', 'Rua da Le');
