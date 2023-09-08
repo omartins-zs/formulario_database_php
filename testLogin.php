2383ab238bbfd96f01aee81c7b4f6bca9dd4772e
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-08 08:28:44 -0300

    Criando Redirects pós Login e Erro no Login

--- testLogin.php
+++ testLogin.php
@@ -7,26 +7,24 @@ if (isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    print_r('Email: ' . $email);
-    print_r('<br>');
-    print_r('Senha: ' . $senha);
+    // print_r('Email: ' . $email);
+    // print_r('<br>');
+    // print_r('Senha: ' . $senha);
 
-    //     $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
 
-    //     $result = $conexao->query($sql);
+    $result = $conexao->query($sql);
 
     //     // print_r($sql);
     //     // print_r($result);
 
-    //     if (mysqli_num_rows($result) < 1) {
-    //         unset($_SESSION['email']);
-    //         unset($_SESSION['senha']);
-    //         header('Location: login.php');
-    //     } else {
-    //         $_SESSION['email'] = $email;
-    //         $_SESSION['senha'] = $senha;
-    //         header('Location: sistema.php');
-    //     }
+    if (mysqli_num_rows($result) < 1) {
+
+        header('Location: login.php');
+    } else {
+
+        header('Location: sistema.php');
+    }
 } else {
     // Não acessa
     header('Location: login.php');
