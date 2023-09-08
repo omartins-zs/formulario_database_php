a954c270bbd425be10ce142dbd2e8fb535495520
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-08 07:57:33 -0300

    Estilizando a View de Login

--- login.php
+++ login.php
@@ -6,7 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tela de login</title>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
+        }
 
+        div {
+            background-color: rgba(0, 0, 0, 0.6);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            padding: 80px;
+            border-radius: 15px;
+            color: #fff;
+        }
+    </style>
 </head>
 
 <body>
